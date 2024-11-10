513eb3970a915aa3e5971ef4e327398841085a0c
drkn-123 <136154911+drkn-123@users.noreply.github.com>
2024-11-10 23:04:49 +0500

    Feat: Agriculture Equipment Routes and Controller

--- routes/dummyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { dummyController /* , dummyController2, dummyController3 */ } = require("../controllers");
-const { authMiddleware /* , middleware2, middleware3 */ } = require("../middlewares");
-
-// router.use(authMiddleware.verifyToken, authMiddleware.verifyFarmer);
-router.get("/", dummyController.dummyFunction);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 // const cookieParser = require("cookie-parser");
 dotenv.config();
-const { dummyRoutes /*, dummyRoutes2, dummyRoutes3 */ } = require("./routes");
+const { equipmentRoutes /*, dummyRoutes2, dummyRoutes3 */ } = require("./routes");
 
 const app = express();
 // app.use(cookieParser());
 app.use(express.json());
 app.use(express.json({ limit: "10mb" })); // if you want to send images or files to the server you need to increase the limit
 app.use(cors({ origin: true, credentials: true }));
-app.use("/dummy", dummyRoutes);
+app.use("/api", equipmentRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Dummy Microservice");
+  res.send("Welcome to the Equipment Rental and Sharing Microservice");
 });
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
